aae6fde2502d1c399f0c8d3e6809fca3dc7dfaf4
brunoschubert <brunoschubertm@gmail.com>
2018-11-19 18:13:36 -0200

    Add different actions

--- Setup/script.js
+++ Setup/script.js
@@ -4,7 +4,8 @@ new Vue({
     data: { 
         playerHealth: 100,
         monsterHealth: 100,
-        isRunning: false
+        isRunning: false,
+        turns: []
     },
 
     methods: {
@@ -12,24 +13,64 @@ new Vue({
             this.isRunning = true;
             this.playerHealth = 100;
             this.monsterHealth = 100;
+            this.turns = [];
         },
         attack: function() {
-
-            this.monsterHealth -= this.calcDMG(3, 10);
+            let damage = this.calcDMG(3, 10);
+            this.monsterHealth -= damage;
+            this.turns.unshift( {
+                isPlayer: true,
+                isHeal: false,
+                isSpecial: false,
+                text: 'Player hits! Dealing ' + damage + ' points of damage.'
+            });
             if(this.isWon()) {
                 return;
             }
-
-            this.playerHealth -= this.calcDMG(5, 12);
-            this.isWon();
+            this.monsterAttack();
         },
         sAttack: function() {
-
+            damage = this.calcDMG(10, 20);
+            this.monsterHealth -= damage;
+            this.turns.unshift( {
+                isPlayer: true,
+                isHeal: false,
+                isSpecial: true,
+                text: "Player Stomps! Dealing " + damage + ' points of damage.'
+            });
+            if(this.isWon()) {
+                return;
+            }
+            this.monsterAttack();
+        },
+        monsterAttack: function() {
+            let damage = this.calcDMG(5, 12);
+            this.playerHealth -= damage;
+            this.turns.unshift( {
+                isPlayer: false,
+                isHeal: false,
+                isSpecial: false,
+                text: 'Monster strikes! Dealing ' + damage + ' points of damage.'
+            });
+            this.isWon();
         },
         heal: function() {
-
+            if (this.playerHealth <= 90) {
+            this.playerHealth +=10;
+        }   else {
+            this.playerHealth = 100;
+        }
+        this.turns.unshift( {
+            isPlayer: true,
+            isHeal: true,
+            isSpecial: false,
+            text: "Player Heals himself for" + 10 + ' points.'
+        });
+            this.monsterAttack();
         },
         giveUp: function() {
+
+            this.isRunning = false;
 
         },
         calcDMG: function(min, max) {
